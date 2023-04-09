578f7d4402da9f170e53ca0b88aa609ee17d57c3
GOGU EDUKONDAL <97788201+goguedukondal@users.noreply.github.com>
2023-04-09 16:04:17 +0530

    Update Context.js

--- src/Components/Context.js
+++ src/Components/Context.js
@@ -4,8 +4,12 @@ export const AuthContext = createContext();
 
 function CustomProvider({ children }) {
   const [isloggedin, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState({
+    name: "",
+    password: "",
+  });
   return (
-    <AuthContext.Provider value={{ isloggedin, setIsLoggedIn }}>
+    <AuthContext.Provider value={{ isloggedin, setIsLoggedIn,user,setUser }}>
       {children}
     </AuthContext.Provider>
   );
